578101a4fefbf725e66793b2a1b57712f2da7740
Lars Sonnenschein <Lars.Sonnenschein@cern.ch>
2014-08-25 12:36:18 +0200

    Rivet 2 version of analysis CMS_2011_I878118 added

---
+++ rivet2/test/rivet_CMS_2011_I878118_cfg.py
@@ -0,0 +1,131 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: Configuration/GenProduction/python/SevenTeV/QCD_Pt_470to600_Tune4C_7TeV_pythia8_cff.py -s GEN --datatier=GEN-SIM-RAW --conditions auto:mc --eventcontent RAWSIM --no_exec -n 10000 --python_filename=rivet_cfg.py --customise=Configuration/GenProduction/rivet_customize.py
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('GEN')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedRealistic8TeVCollision_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(10000)
+)
+
+# Input source
+process.source = cms.Source("EmptySource")
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('\\$Revision: 1.1 $'),
+    annotation = cms.untracked.string('Sample with PYTHIA8: QCD dijet production, pThat = 470 .. 600 GeV, Tune4C'),
+    name = cms.untracked.string('\\$Source: /local/reps/CMSSW/CMSSW/Configuration/GenProduction/python/SevenTeV/QCD_Pt_470to600_Tune4C_7TeV_pythia8_cff.py,v $')
+)
+
+# Output definition
+
+process.RAWSIMoutput = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    outputCommands = process.RAWSIMEventContent.outputCommands,
+    fileName = cms.untracked.string('QCD_Pt_470to600_Tune4C_7TeV_pythia8_cff_py_GEN.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('GEN-SIM-RAW')
+    ),
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    )
+)
+
+# Additional output definition
+
+# Other statements
+process.genstepfilter.triggerConditions=cms.vstring("generation_step")
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:mc', '')
+
+process.generator = cms.EDFilter("Pythia8GeneratorFilter",
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    filterEfficiency = cms.untracked.double(1),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(7000.0),
+    crossSection = cms.untracked.double(77.03948),
+    maxEventsToPrint = cms.untracked.int32(0),
+    PythiaParameters = cms.PSet(
+        processParameters = cms.vstring('Main:timesAllowErrors    = 10000', 
+            'ParticleDecays:limitTau0 = on', 
+            'ParticleDecays:tauMax = 10', 
+            # 'HardQCD:all = on', 
+	    'HardQCD:hardbbbar = on',
+            'PhaseSpace:pTHatMin = 470 ', 
+            'PhaseSpace:pTHatMax = 600 ', 
+            'Tune:pp 5', 
+            'Tune:ee 3'),
+        parameterSets = cms.vstring('processParameters')
+    )
+)
+
+process.bfilter = cms.EDFilter(
+"PythiaFilter",
+MaxEta = cms.untracked.double(9999.),
+MinEta = cms.untracked.double(-9999.),
+ParticleID = cms.untracked.int32(521)
+)
+
+process.jpsifilter = cms.EDFilter(
+"PythiaDauVFilter",
+verbose = cms.untracked.int32(0),
+NumberDaughters = cms.untracked.int32(2),
+MotherID = cms.untracked.int32(521),
+ParticleID = cms.untracked.int32(443),
+DaughterIDs = cms.untracked.vint32(13, -13),
+MinPt = cms.untracked.vdouble(0., 0.),
+MinEta = cms.untracked.vdouble(-99999., -99999.),
+MaxEta = cms.untracked.vdouble( 99999., 99999.)
+)
+
+
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.RAWSIMoutput_step = cms.EndPath(process.RAWSIMoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.endjob_step,process.RAWSIMoutput_step)
+# filter all path with the production filter sequence
+for path in process.paths:
+	getattr(process,path)._seq = process.generator * getattr(process,path)._seq 
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from Configuration.GenProduction.rivet_customize
+from Configuration.GenProduction.rivet_customize import customise 
+
+#call to customisation function customise imported from Configuration.GenProduction.rivet_customize
+process = customise(process)
+#process = customise(process*bfilter*jpsifilter)
+
+# End of customisation functions
+#process.rivetAnalyzer.AnalysisNames = cms.vstring('CMS_2013_I1224539_DIJET')
+process.rivetAnalyzer.AnalysisNames = cms.vstring('CMS_2011_I878118')
+process.rivetAnalyzer.OutputFile = cms.string('mcfile.yoda')
